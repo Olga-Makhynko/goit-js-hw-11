56008ea888acf12f5b048b8c1bfc74e1ddbdb63a
Olga <olichka7891olichka@gmail.com>
2025-04-17 16:26:40 +0100

    correction pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,24 +1,17 @@
 import axios from 'axios';
 
-export async function getImagesByQuery(query) {
-  const ApiKey = '49778514-c989ee5c1575b477a31bb7b72';
-  return axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: ApiKey,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 100,
-      },
-    })
-    .then(response => {
-      return response.data.hits;
-    })
+const ApiKey = '49778514-c989ee5c1575b477a31bb7b72';
+const BASE_URL = 'https://pixabay.com/api/';
 
-    .catch(error => {
-      console.error(error);
-      throw error;
-    });
+export async function getImagesByQuery(query) {
+  const params = {
+    key: ApiKey,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
 }
