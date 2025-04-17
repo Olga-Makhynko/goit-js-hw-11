ce572777127abaf4cfb505079d2a309bef409906
Olga <olichka7891olichka@gmail.com>
2025-04-17 16:39:20 +0100

    add render functions js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,3 +5,45 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+
+export function createGallery(images) {
+  const galleryList = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+            <a href="${largeImageURL}">
+              <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+            </a>
+            <div class="info">
+              <p> ${likes}</p>
+              <p> ${views}</p>
+              <p> ${comments}</p>
+              <p> ${downloads}</p>
+            </div>
+          </li>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', galleryList);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('visible');
+}
