f22040301bc2b05b071031f4a29a45845c37aab1
Olga <olichka7891olichka@gmail.com>
2025-04-17 16:00:01 +0100

    add index html

---
+++ src/js/render-functions.js
@@ -0,0 +1,5 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
